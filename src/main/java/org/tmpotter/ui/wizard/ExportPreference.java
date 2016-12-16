250e245c5800f99180bb45ad4fc4340d030e9aa9
Hiroshi Miura <miurahr@linux.com>
2016-12-17 04:55:30 +0900

    Introduce Export Wizard
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

---
+++ src/main/java/org/tmpotter/ui/wizard/ExportPreference.java
@@ -0,0 +1,51 @@
+/* *************************************************************************
+ *
+ *  TMPotter - Bi-text Aligner/TMX Editor
+ *
+ *  Copyright (C) 2016 Hiroshi Miura
+ *
+ *  This file is part of TMPotter.
+ *
+ *  TMPotter is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  TMPotter is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ * 
+ *  You should have received a copy of the GNU General Public License
+ *  along with TMPotter.  If not, see http://www.gnu.org/licenses/.
+ *
+ * *************************************************************************/
+
+package org.tmpotter.ui.wizard;
+
+/**
+ * Export preference.
+ * @author Hiroshi Miura
+ */
+public class ExportPreference {
+	private String filePath;
+	private String filter;
+
+	public String getFilePath() {
+		return filePath;
+	}
+
+	public void setFilePath(String filePath) {
+		this.filePath = filePath;
+	}
+
+	public String getFilter() {
+		return filter;
+	}
+
+	public void setFilter(String filter) {
+		this.filter = filter;
+	}
+	
+	
+}
